6b0c258e83cdd0be35ef3e8d37f7e53edd03590f
Cameron Anderson and Nicko Dela Cruz <cameron@>
2018-01-23 14:46:39 -0800

    style with css

--- js/scripts.js
+++ js/scripts.js
@@ -12,16 +12,48 @@ Account.prototype.deposit = function(amount) {
 };
 
 
+function updateBalance (amount) {
+  $("#balance").text(amount)
+}
 
-
+var accounts = Array(0);
 var account;
 $(document).ready(function() {
   $("#new-account").submit(function(event) {
     event.preventDefault();
 
     account = new Account($("#name").val(), parseInt($("#initialDeposit").val()));
-    console.log(account);
+    $("#name").val("")
+    $("#initialDeposit").val("")
+    updateBalance(account.balance);
 
-    account.withdraw(23412);
+    $("#accounts").append("<option value='" + accounts.length + "'>" + account.name + "</option>");
+    $("#accounts").val(accounts.length);
+    accounts.push(account);
+  });
+
+  $("#existing-account").submit(function(event) {
+    event.preventDefault();
+
+    var value = $("#withdraw").val();
+    if (value !== "") {
+      value = parseInt(value);
+      if (value <= account.balance)
+        account.withdraw(value);
+    }
+    value = $("#deposit").val();
+    if (value !== "") {
+      value = parseInt(value);
+      account.deposit(value);
+    }
+    $("#withdraw").val("");
+    $("#deposit").val("");
+    updateBalance(account.balance);
+
+  });
+
+  $("#accounts").change(function(){
+    account = accounts[parseInt($(this).val())];
+    updateBalance(account.balance);
   });
 });
